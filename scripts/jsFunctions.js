b61f1ff61aabf2c73ec1143438a036fd27b8e52c
root <root@spreyo-windows.localdomain>
2022-09-05 16:12:14 +0200

    initial commit

---
+++ scripts/jsFunctions.js
@@ -0,0 +1,37 @@
+function showAlert(message) {
+    console.log(message)
+}
+
+const radios = {};
+
+function muteRadio(radioName) {
+    const radio = radios[radioName];
+    radio.muted = true;
+}
+
+function unmuteRadio(radioName) {
+    const radio = radios[radioName];
+    radio.muted = false;
+    console.log(`${radioName} muted ${radio.muted}`)
+}
+
+// function playRadio(radio) {
+//     const europa2 = new Audio("https://stream.bauermedia.sk/europa2.mp3")
+//     console.log("playing audio")
+//     // europa2.muted = true;
+//     europa2.id = "europa2"
+//     europa2.play();
+// }
+
+function newRadio(radioName, audioStream) {
+    const radio = new Audio(audioStream);
+    radio.id = radioName;
+    // radio.defaultMuted = true;
+    radio.muted = true;
+    radios[radioName] = radio;
+    radio.play();
+    console.log("new radio " + radioName + ' ' + audioStream);
+    console.log(radios)
+}
+
+
